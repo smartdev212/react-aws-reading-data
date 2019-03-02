3871b68af2f39523deda3a4b5b112629d39a6d55
smartdev212 <smartdev212@gmail.com>
2019-03-01 21:51:34 -0500

    upgrade react version

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -13,78 +13,78 @@ import scroll from '../ScrollToTop/scroll'
 
 const mql = window.matchMedia(`(min-width: ${breakpoints.small}rem)`)
 export default class App extends Component<{}, AppState> {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props)
 
-        this.receiveBooks = this.receiveBooks.bind(this)
-        this.toggleSidebar = this.toggleSidebar.bind(this)
+    this.receiveBooks = this.receiveBooks.bind(this)
+    this.toggleSidebar = this.toggleSidebar.bind(this)
 
-        this.state = {
-            books: [],
-            stats: null,
-            filterOptions: null,
-            mql,
-            sidebarOpen: false,
-            sidebarDocked: true
-        }
-
-        this.mediaQueryChanged = this.mediaQueryChanged.bind(this)
-        this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this)
+    this.state = {
+      books: [],
+      stats: null,
+      filterOptions: null,
+      mql,
+      sidebarOpen: false,
+      sidebarDocked: true
     }
 
-    public render(): JSX.Element {
-        return (
-            <AppContainer>
-                <ReactSidebar
-                    sidebar={
-                        <Sidebar
-                            onChange={this.receiveBooks}
-                            open={this.state.sidebarOpen}
-                            toggleSidebar={this.toggleSidebar}
-                        />
-                    }
-                    docked={this.state.sidebarDocked}
-                    open={this.state.sidebarOpen}
-                    onSetOpen={this.onSetSidebarOpen}
-                >
-                    <Header toggleSidebar={this.toggleSidebar} />
-                    <BodyContainer id="body-container">
-                        <BookList books={this.state.books} />
-                        <ScrollToTop />
-                    </BodyContainer>
-                </ReactSidebar>
-            </AppContainer>
-        )
-    }
+    this.mediaQueryChanged = this.mediaQueryChanged.bind(this)
+    this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this)
+  }
 
-    public componentWillMount() {
-        mql.addListener(this.mediaQueryChanged)
-        this.setState({ mql: mql, sidebarDocked: mql.matches })
-    }
+  public render(): JSX.Element {
+    return (
+      <AppContainer>
+        <ReactSidebar
+          sidebar={
+            <Sidebar
+              onChange={this.receiveBooks}
+              open={this.state.sidebarOpen}
+              toggleSidebar={this.toggleSidebar}
+            />
+          }
+          docked={this.state.sidebarDocked}
+          open={this.state.sidebarOpen}
+          onSetOpen={this.onSetSidebarOpen}
+        >
+          <Header toggleSidebar={this.toggleSidebar} />
+          <BodyContainer id="body-container">
+            <BookList books={this.state.books} />
+            <ScrollToTop />
+          </BodyContainer>
+        </ReactSidebar>
+      </AppContainer>
+    )
+  }
 
-    public componentWillUnmount() {
-        this.state.mql.removeListener(this.mediaQueryChanged)
-    }
+  public componentWillMount() {
+    mql.addListener(this.mediaQueryChanged)
+    this.setState({ mql: mql, sidebarDocked: mql.matches })
+  }
 
-    public receiveBooks(
-        books: Book[],
-        stats: IStats,
-        filterOptions: FilterOptions
-    ) {
-        scroll()
-        this.setState({ books, stats, filterOptions })
-    }
+  public componentWillUnmount() {
+    this.state.mql.removeListener(this.mediaQueryChanged)
+  }
 
-    private toggleSidebar() {
-        this.setState({ sidebarOpen: !this.state.sidebarOpen })
-    }
+  public receiveBooks(
+    books: Book[],
+    stats: IStats,
+    filterOptions: FilterOptions
+  ) {
+    scroll()
+    this.setState({ books, stats, filterOptions })
+  }
 
-    private mediaQueryChanged() {
-        this.setState({ sidebarDocked: this.state.mql.matches })
-    }
+  private toggleSidebar() {
+    this.setState({ sidebarOpen: !this.state.sidebarOpen })
+  }
 
-    private onSetSidebarOpen() {
-        const sidebarOpen = arguments[0]
-        this.setState({ sidebarOpen })
-    }
+  private mediaQueryChanged() {
+    this.setState({ sidebarDocked: this.state.mql.matches })
+  }
+
+  private onSetSidebarOpen() {
+    const sidebarOpen = arguments[0]
+    this.setState({ sidebarOpen })
+  }
 }
--- src/components/BookList/test.tsx
+++ src/components/BookList/test.tsx
@@ -5,58 +5,58 @@ import BookList from './BookList'
 import { generateBookList } from '../../../test-utils/data-factory'
 
 const getReviewEls = (book: Element) => ({
-    reviewToggle: book.getElementsByClassName('review-toggle').item(0),
-    review: book.getElementsByClassName('book-review').item(0)
+  reviewToggle: book.getElementsByClassName('review-toggle').item(0),
+  review: book.getElementsByClassName('book-review').item(0)
 })
 
 const expectReviewHidden = (review: Element) =>
-    expect(getComputedStyle(review).maxHeight).toBe('0')
+  expect(getComputedStyle(review).maxHeight).toBe('0')
 const expectReviewShown = (review: Element) =>
-    expect(getComputedStyle(review).maxHeight).not.toBe('0')
+  expect(getComputedStyle(review).maxHeight).not.toBe('0')
 
 describe('Book', () => {
-    it('renders a list of books', async () => {
-        const books = await generateBookList(10)
-        const { getByTestId } = render(<BookList books={books} />)
+  it('renders a list of books', async () => {
+    const books = await generateBookList(10)
+    const { getByTestId } = render(<BookList books={books} />)
 
-        books.forEach(book => {
-            getByTestId(book['Book Id'].toString())
-        })
+    books.forEach(book => {
+      getByTestId(book['Book Id'].toString())
     })
+  })
 
-    it('shows a review', async () => {
-        const books = await generateBookList(10)
-        const thirdBook = books[2]
+  it('shows a review', async () => {
+    const books = await generateBookList(10)
+    const thirdBook = books[2]
 
-        const { getByTestId } = render(<BookList books={books} />)
+    const { getByTestId } = render(<BookList books={books} />)
 
-        const book = getByTestId(thirdBook['Book Id'].toString())
-        const { review, reviewToggle } = getReviewEls(book)
+    const book = getByTestId(thirdBook['Book Id'].toString())
+    const { review, reviewToggle } = getReviewEls(book)
 
-        expectReviewHidden(review)
+    expectReviewHidden(review)
 
-        fireEvent.click(reviewToggle)
+    fireEvent.click(reviewToggle)
 
-        expectReviewShown(review)
-    })
+    expectReviewShown(review)
+  })
 
-    it('only shows 1 review at a time', async () => {
-        const books = await generateBookList(10)
-        const { getByTestId } = render(<BookList books={books} />)
+  it('only shows 1 review at a time', async () => {
+    const books = await generateBookList(10)
+    const { getByTestId } = render(<BookList books={books} />)
 
-        const firstBook = getByTestId(books[0]['Book Id'].toString())
-        const secondBook = getByTestId(books[1]['Book Id'].toString())
+    const firstBook = getByTestId(books[0]['Book Id'].toString())
+    const secondBook = getByTestId(books[1]['Book Id'].toString())
 
-        const firstReview = getReviewEls(firstBook)
-        const secondReview = getReviewEls(secondBook)
+    const firstReview = getReviewEls(firstBook)
+    const secondReview = getReviewEls(secondBook)
 
-        expectReviewHidden(firstReview.review)
-        expectReviewHidden(secondReview.review)
+    expectReviewHidden(firstReview.review)
+    expectReviewHidden(secondReview.review)
 
-        fireEvent.click(firstReview.reviewToggle)
-        fireEvent.click(secondReview.reviewToggle)
+    fireEvent.click(firstReview.reviewToggle)
+    fireEvent.click(secondReview.reviewToggle)
 
-        expectReviewHidden(firstReview.review)
-        expectReviewShown(secondReview.review)
-    })
+    expectReviewHidden(firstReview.review)
+    expectReviewShown(secondReview.review)
+  })
 })
