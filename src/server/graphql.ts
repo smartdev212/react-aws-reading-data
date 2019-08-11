e63090c47e58cc23d9b3d8d6abc21ec3b53019d0
Nathan Griffin <nathangriffin@Nathans-MBP.lan>
2019-08-10 23:02:52 -0400

    graphql endpdoint working

--- src/server/graphql.ts
+++ src/server/graphql.ts
@@ -1,24 +1,39 @@
 import { ApolloServer, gql } from 'apollo-server-lambda'
+import AWS from 'aws-sdk'
+
+const dynamo = new AWS.DynamoDB.DocumentClient()
 
 const typeDefs = gql`
+  type Book {
+    id: String
+    book_id: Int
+    title: String
+    author: String
+    my_rating: Int
+    number_of_pages: Int
+    date_read: String
+    my_review: String
+    isbn: String
+    isbn_13: String
+  }
+
   type Query {
-    hello: String
+    getBooks: [Book!]!
   }
 `
 
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!'
+    getBooks: async () => {
+      const result = await dynamo.scan({ TableName: 'Books' }).promise()
+      return result.Items
+    }
   }
 }
 
-const server = new ApolloServer({
+export const server = new ApolloServer({
   typeDefs,
   resolvers,
   playground: true,
   introspection: true
-})
-
-export const handler = server.createHandler({
-  cors: { origin: '*', credentials: false }
 })
