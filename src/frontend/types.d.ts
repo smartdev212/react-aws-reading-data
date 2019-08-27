b8e2b80def5ec607bf8bbd07305aa675d8c40268
smartdev212 <smartdev212@gmail.com>
2019-08-27 00:25:52 -0400

    move data & types around

--- src/frontend/types.d.ts
+++ src/frontend/types.d.ts
@@ -1,16 +1,3 @@
-export type BookConversion = Pick<
-  GoodreadsBook,
-  | 'Book Id'
-  | 'Title'
-  | 'Author'
-  | 'My Rating'
-  | 'Number of Pages'
-  | 'Date Read'
-  | 'My Review'
-  | 'ISBN'
-  | 'ISBN13'
->
-
 export interface Book {
   id: string
   book_id: number
@@ -22,40 +9,6 @@ export interface Book {
   my_review: string
   isbn?: string
   isbn_13?: string
-}
-
-export interface GoodreadsBook {
-  'Book Id': number
-  Title: string
-  Author: string
-  'Author l-f': string
-  'Additional Authors': string
-  ISBN: string
-  ISBN13: string
-  'My Rating': number
-  'Average Rating': number
-  Publisher: string
-  Binding: string
-  'Number of Pages': number
-  'Year Published': number
-  'Original Publication Year': number
-  'Date Read': string
-  'Date Added': string
-  Bookshelves: string
-  'Bookshelves with positions': string
-  'Exclusive Shelf': string
-  'My Review': string
-  Spoiler: string
-  'Private Notes': string
-  'Read Count': number
-  'Recommended For': string
-  'Recommended By': string
-  'Owned Copies': string | number
-  'Original Purchase Date': string
-  'Original Purchase Location': string
-  Condition: string
-  'Condition Description': string
-  BCID: string
 }
 
 export interface Stats {
