e263b728033482693e7c7390bebd564c6d385098
smartdev212 <smartdev212@gmail.com>
2019-09-02 07:12:04 -0400

    add lerna + massive folder reorg

--- packages/server/src/types.d.ts
+++ packages/server/src/types.d.ts
@@ -1,4 +1,15 @@
-import { Book } from '../frontend/types'
+interface Book {
+  id: string
+  book_id: number
+  title: string
+  author: string
+  my_rating: number
+  number_of_pages: number
+  date_read: string
+  my_review: string
+  isbn?: string
+  isbn_13?: string
+}
 
 export type BookConversion = Pick<
   GoodreadsBook,
