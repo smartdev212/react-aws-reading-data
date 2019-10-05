479051cbc1080f4b990a82abdcc80db7cbd5278a
smartdev212 <smartdev212@gmail.com>
2019-10-05 11:08:07 -0400

    upgrade emotion

--- packages/frontend/src/components/BookList/NoBooks/styles.ts
+++ packages/frontend/src/components/BookList/NoBooks/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'react-emotion'
+import styled from '@emotion/styled'
 
 import { queries } from '../../../shared/breakpoints'
 const bookPic = require('./no_books.jpg')
--- packages/frontend/src/components/Elements/Button/styles.ts
+++ packages/frontend/src/components/Elements/Button/styles.ts
@@ -1,4 +1,4 @@
-import styled from 'react-emotion'
+import styled from '@emotion/styled'
 
 import { primary, white } from '../../../design/colors'
 import { sanSerif } from '../../../design/fonts'
